b4419465050597e4f27e3d4f56c36b5bb2c61d24
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-13 15:45:10 +0100

    total cash test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,22 @@
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+def total_cash(sum)
+  return sum[:admin][:total_cash]
+end
+
+
+# def test_add_or_remove_cash__add
+#   add_or_remove_cash(@pet_shop,10)
+#   cash = total_cash(@pet_shop)
+#   assert_equal(1010, cash)
+# end
+#
+# def add_or_remove_cash(shop,number)
+#   return shop[:admin][:total_cash]+= number
+# end
+#
+# def add_or_remove_cash(shop,number)
+#   return shop[:admin][:total_cash]-= number
+# end
