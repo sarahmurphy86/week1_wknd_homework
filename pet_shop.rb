db902b3a5bf5019d425c9f6912d4c5786bc1bfab
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-14 15:51:10 +0100

    stock count passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,18 +2,33 @@ def pet_shop_name(shop)
   return shop[:name]
 end
 
-def total_cash(sum)
-  return sum[:admin][:total_cash]
+def total_cash(shop)
+  return shop[:admin][:total_cash]
 end
 
-#
-# def add_or_remove_cash(shop,number)
-#   return shop[:admin][:total_cash]+= number
+# Review the two below - I am not sure these are correct
+def add_or_remove_cash(shop,amount)
+  return shop[:admin][:total_cash]+= amount
+end
+
+# def add_or_remove_cash(shop,amount)
+#   return shop[:admin][:total_cash]-= amount
 # end
+
+def pets_sold(amount)
+  return amount[:admin][:pets_sold]
+end
 #
-# def add_or_remove_cash(shop,number)
-#   return shop[:admin][:total_cash]-= number
+# # The one below works - I am not sure how? - I think we need to define the amount?
+def increase_pets_sold (amount,increase)
+  return amount[:admin][:pets_sold]+=increase
+end
+
+# def test_stock_count
+#   count = stock_count(@pet_shop)
+#   assert_equal(6, count)
 # end
-def pets_sold(total)
-  return total[:admin][:pets_sold]
+
+def stock_count(amount)
+  return amount[:pets].count
 end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -83,12 +83,12 @@ class TestPetShop < Minitest::Test
     assert_equal(1000, sum)
   end
 
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(1010, cash)
-  # end
-  #
+  def test_add_or_remove_cash__add
+    add_or_remove_cash(@pet_shop,10)
+    cash = total_cash(@pet_shop)
+    assert_equal(1010, cash)
+  end
+
   # def test_add_or_remove_cash__remove
   #   add_or_remove_cash(@pet_shop,-10)
   #   cash = total_cash(@pet_shop)
@@ -99,17 +99,17 @@ class TestPetShop < Minitest::Test
     sold = pets_sold(@pet_shop)
     assert_equal(0, sold)
   end
-
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
-
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  #
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
+  #
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
