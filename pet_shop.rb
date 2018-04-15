d4257a92c9c8e1a374632755917e8152a8868a11
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-15 20:42:00 +0100

    ten tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -23,8 +23,7 @@ end
 def stock_count(amount)
   return amount[:pets].count
 end
-#
-# # So I want to search through the breeds in the pet shop - need a for loop. If I get a match with the given breed I want to count it. So if I get a match I will pass the match into an array and then count up how many items in the array.I think shop needs to be included somwewhere as it is part of the argument but I dont know where?
+
 def pets_by_breed(shop,type)
   total = []
   for pet in shop[:pets]
@@ -35,13 +34,14 @@ end
 
 # Type in "arthur" we look through all the pet names in the pet shop - need a for loop. If I get a name match, I want to return that pet hash. I think this should also work for the Fred example and return nil.
 
-# def find_pet_by_name (pets, name)
-#   for pet in pets[:pets]
-#     if (pet[:name]==name)
-#       return pet
-#     end
-#   end
-# end
+def find_pet_by_name (shop,pet_name)
+  for pet in shop[:pets]
+    if pet[:name] == pet_name
+      return pet
+      # else
+    end
+  end
+end
 
 # remove a pet by name e.g called Athur, so when you search for it you should get a nil response
 # def remove_pet_by_name(shop, name)
